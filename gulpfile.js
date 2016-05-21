f5918ae0749555327740538480dd0c060714ac24
Adam Cavendish <GetbetterABC@yeah.net>
2016-05-22 01:32:32 +0800

    Fix build not minified issue, and concat to one min file

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var concat = require('gulp-concat');
-//var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
   return gulp.src('src/*.js')
     .pipe(sourcemaps.init())
-    //.pipe(babel())
     .pipe(concat('all.js'))
-    .pipe(sourcemaps.write('.'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(rename('all.min.js'))
     .pipe(gulp.dest('dist/src'));
 });
 
 gulp.task('build-test', function () {
-  return gulp.src('test/*.js')
+  return gulp.src('test/test.js')
     .pipe(sourcemaps.init())
-    //.pipe(babel())
+    .pipe(concat('test.js'))
+    //.pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(rename('test.min.js'))
     .pipe(gulp.dest('dist/test'));
 });
 
 gulp.task('test', ['default', 'build-test'], function () {
   var mocha = require('gulp-mocha');
-/*
-  return gulp.src("dist/test/*.js", {read:false})
-    .pipe(mocha());
-*/
-  return gulp.src('dist/test/test.js').pipe(mocha());
+  return gulp.src('dist/test/test.min.js').pipe(mocha());
 });
